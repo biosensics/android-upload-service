5527a578c58a4d81d0c98693923b95051537caf7
Borna.Fatehi <borna.fatehi@biosensics.com>
2020-12-24 15:40:32 -0500

    spacing issue

--- uploadservice-s3/src/main/java/net/gotev/uploadservice/s3/S3Setup.kt
+++ uploadservice-s3/src/main/java/net/gotev/uploadservice/s3/S3Setup.kt
@@ -1,14 +1,8 @@
 package net.gotev.uploadservice.s3
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferService
-import java.util.UUID
 
 class S3Setup(context: Context) {
     private val context = context
@@ -17,6 +11,6 @@ class S3Setup(context: Context) {
      * You should call this once. It can be either on activity create or the first time you want to do the upload.
      */
     fun startTransferService() {
-          context.startService(Intent(context, TransferService::class.java))
-      }
+        context.startService(Intent(context, TransferService::class.java))
+    }
 }
